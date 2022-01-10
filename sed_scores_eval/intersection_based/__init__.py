ac072ca06b93537e1d71c5b6a2e80862cce7835a
Janek Ebbers <ebbers@nt.upb.de>
2022-01-10 18:35:34 +0100

    add intersection_based.precision_recall

--- sed_scores_eval/intersection_based/__init__.py
+++ sed_scores_eval/intersection_based/__init__.py
@@ -1,3 +1,4 @@
 from .intermediate_statistics import intermediate_statistics
 from .psds import psd_roc, psds
+from .precision_recall import precision_recall_curve, fscore_curve, fscore, best_fscore
 from . import reference
