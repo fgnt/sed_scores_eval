a31a58ce964e9ec8e4ce9b55769f591fe7638eaf
Janek Ebbers <ebbers@nt.upb.de>
2022-01-10 18:40:28 +0100

    add comparison tests using reference packages (psds_eval & sed_eval) on test data

---
+++ tests/test_collar_based/test_collar_based_fscore.py
@@ -0,0 +1,53 @@
+import pytest
+import numpy as np
+from sed_scores_eval import collar_based, io, package_dir
+from sed_scores_eval.base_modules.precision_recall import fscore_from_sed_eval_metrics
+
+
+@pytest.mark.parametrize("dataset", ["validation", "eval"])
+@pytest.mark.parametrize(
+    "threshold",
+    [
+        .5,
+        {
+            'Alarm_bell_ringing': 0.9713,
+            'Blender': 0.7718,
+            'Cat': 0.8573,
+            'Dishes': 0.9535,
+            'Dog': 0.9495,
+            'Electric_shaver_toothbrush': 0.9616,
+            'Frying': 0.7752,
+            'Running_water': 0.9555,
+            'Speech': 0.8314,
+            'Vacuum_cleaner': 0.6164
+        }
+    ]
+)
+@pytest.mark.parametrize("collar", [.2, .5])
+def test_collar_based_fscore_vs_sed_eval(dataset, threshold, collar):
+    offset_collar_rate = collar
+    time_decimals = 30
+    test_data_dir = package_dir / 'tests' / 'data'
+    if not test_data_dir.exists():
+        io.download_test_data()
+
+    f, p, r, stats = collar_based.fscore(
+        scores=test_data_dir / dataset / "scores",
+        ground_truth=test_data_dir / dataset / "ground_truth.tsv",
+        threshold=threshold,
+        onset_collar=collar, offset_collar=collar,
+        offset_collar_rate=offset_collar_rate,
+        time_decimals=time_decimals,
+    )
+    sed_eval_metrics = collar_based.reference.metrics(
+        scores=test_data_dir / dataset / "scores",
+        ground_truth=test_data_dir / dataset / "ground_truth.tsv",
+        threshold=threshold,
+        collar=collar, offset_collar_rate=offset_collar_rate,
+    )
+    f_sed_eval, p_sed_eval, r_sed_eval = fscore_from_sed_eval_metrics(
+        sed_eval_metrics)
+    for key in f.keys():
+        np.testing.assert_almost_equal(f[key], f_sed_eval[key])
+        np.testing.assert_almost_equal(p[key], p_sed_eval[key])
+        np.testing.assert_almost_equal(r[key], r_sed_eval[key])
---
+++ tests/test_intersection_based/test_psds.py
@@ -0,0 +1,90 @@
+import pytest
+import numpy as np
+from scipy.interpolate import interp1d
+from sed_scores_eval import intersection_based, io, package_dir
+
+
+@pytest.mark.parametrize("dataset", ["validation", "eval"])
+@pytest.mark.parametrize(
+    "params",
+    [
+        {
+            'dtc_threshold': .7,
+            'gtc_threshold': .7,
+            'cttc_threshold': None,
+            'alpha_ct': .0,
+            'alpha_st': 1 / np.sqrt(10 - 1) - 1e-9,
+        },
+        {
+            'dtc_threshold': .1,
+            'gtc_threshold': .1,
+            'cttc_threshold': .3,
+            'alpha_ct': .5,
+            'alpha_st': 1 / np.sqrt(10 - 1) - 1e-9,
+        },
+    ]
+)
+def test_psds_vs_psds_eval(dataset, params):
+
+    test_data_dir = package_dir / 'tests' / 'data'
+    if not test_data_dir.exists():
+        io.download_test_data()
+
+    psds, psd_roc, single_class_psd_rocs = intersection_based.psds(
+        scores=test_data_dir / dataset / "scores",
+        ground_truth=test_data_dir / dataset / "ground_truth.tsv",
+        audio_durations=test_data_dir / dataset / "audio_durations.tsv",
+        dtc_threshold=params['dtc_threshold'],
+        gtc_threshold=params['gtc_threshold'],
+        cttc_threshold=params['cttc_threshold'],
+        alpha_ct=params['alpha_ct'], alpha_st=params['alpha_st'],
+        unit_of_time='hour', max_efpr=100.,
+        time_decimals=30,
+    )
+
+    score_transform = test_data_dir / 'validation' / 'score_transform.tsv'
+    if not score_transform.exists():
+        io.write_score_transform(
+            scores=test_data_dir / 'validation' / "scores",
+            ground_truth=test_data_dir / 'validation' / "ground_truth.tsv",
+            filepath=test_data_dir / 'validation' / 'score_transform.tsv'
+        )
+
+    (
+        psds_approx, psd_roc_approx, single_class_psd_rocs_approx
+    ) = intersection_based.reference.approximate_psds(
+        scores=test_data_dir / dataset / "scores",
+        ground_truth=test_data_dir / dataset / "ground_truth.tsv",
+        audio_durations=test_data_dir / dataset / "audio_durations.tsv",
+        thresholds=np.linspace(0.001, 0.999, 500),
+        dtc_threshold=params['dtc_threshold'],
+        gtc_threshold=params['gtc_threshold'],
+        cttc_threshold=params['cttc_threshold'],
+        alpha_ct=params['alpha_ct'], alpha_st=params['alpha_st'],
+        unit_of_time='hour', max_efpr=100.,
+        score_transform=score_transform,
+    )
+    assert 1e-2 > (psds - psds_approx) >= 0., (psds, psds_approx)
+
+    def assert_true_roc_geq_approx_roc(roc_true, roc_approx, class_name=''):
+        tpr_true, fpr_true, *_ = roc_true
+        fpr_true = np.round(fpr_true, 6)
+        tpr_approx, fpr_approx, *_ = roc_approx
+        fpr_approx = np.round(fpr_approx, 6)
+        tpr_true = interp1d(
+            fpr_true, tpr_true, kind='previous',
+            bounds_error=False, fill_value=(0, tpr_true[-1])
+        )(fpr_approx)
+        assert (tpr_true >= tpr_approx).all(), (
+            class_name,
+            np.sum(tpr_true < tpr_approx),
+            (tpr_approx - tpr_true).max(),
+        )
+
+    assert_true_roc_geq_approx_roc(psd_roc, psd_roc_approx, 'psd-roc')
+    for event_class in single_class_psd_rocs:
+        assert_true_roc_geq_approx_roc(
+            single_class_psd_rocs[event_class],
+            single_class_psd_rocs_approx[event_class],
+            event_class,
+        )
