6c610e572421fbacd3080ff42b74dd44f97fd631
Janek Ebbers <ebbers@nt.upb.de>
2022-05-16 21:43:17 +0200

    introduce approximate_psds_from_detections_dir

--- sed_scores_eval/intersection_based/reference.py
+++ sed_scores_eval/intersection_based/reference.py
@@ -66,28 +66,83 @@ def approximate_psds(
 
     """
     import tempfile
-    from psds_eval import PSDSEval
     scores, _, keys = parse_inputs(scores, ground_truth)
-    if isinstance(ground_truth, dict):
-        ground_truth = [(f"{key}.wav", *gt) for key in keys for gt in ground_truth[key]]
-        ground_truth = pd.DataFrame(
-            ground_truth,
-            columns=['filename', 'onset', 'offset', 'event_label']
-        )
-    else:
-        assert isinstance(ground_truth, (str, Path)), type(ground_truth)
-        ground_truth = pd.read_csv(ground_truth, sep="\t")
+    ground_truth = _parse_ground_truth(ground_truth)
     audio_format = ground_truth['filename'][0].split('.')[-1]
-    if isinstance(audio_durations, dict):
-        audio_durations = [(f"{key}.wav", audio_durations[key]) for key in keys]
-        audio_durations = pd.DataFrame(
-            audio_durations, columns=['filename', 'duration']
+    validate_score_dataframe(scores[keys[0]])
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        dir_path = Path(tmp_dir)
+        write_detections_for_multiple_thresholds(
+            scores, thresholds, dir_path, audio_format=audio_format,
+            score_transform=score_transform,
         )
-    else:
-        assert isinstance(audio_durations, (str, Path)), type(audio_durations)
-        audio_durations = pd.read_csv(audio_durations, sep="\t")
-    _, event_classes = validate_score_dataframe(
-        scores[keys[0]])
+        return approximate_psds_from_detections_dir(
+            dir_path, ground_truth, audio_durations, thresholds,
+            dtc_threshold=dtc_threshold, gtc_threshold=gtc_threshold,
+            cttc_threshold=cttc_threshold,
+            alpha_ct=alpha_ct, alpha_st=alpha_st,
+            unit_of_time=unit_of_time, max_efpr=max_efpr,
+        )
+
+
+def approximate_psds_from_detections_dir(
+        dir_path, ground_truth, audio_durations,
+        thresholds=np.linspace(.01, .99, 50), *,
+        dtc_threshold, gtc_threshold, cttc_threshold=None,
+        alpha_ct=.0, alpha_st=.0, unit_of_time='hour', max_efpr=100.,
+):
+    """Reference psds implementation using the psds_eval package
+    (https://github.com/audioanalytic/psds_eval), which, however, only
+    approximates the PSD-ROC using a limited set of thresholds/operating points.
+    This function is primarily used for testing purposes.
+
+    Args:
+        dir_path (str, pathlib.Path): path to directory where the detections
+            for each threshold are saved.
+        ground_truth (dict, str or pathlib.Path): dict of lists of ground truth
+            event tuples (onset, offset, event label) for each audio clip or a
+            file path from where the ground truth can be loaded.
+        audio_durations: The duration of each audio file in the evaluation set.
+        thresholds: the set of thresholds used to approximate the PSD-ROC.
+        dtc_threshold (float): detection tolerance criterion threshold
+        gtc_threshold (float): ground truth intersection criterion threshold
+        cttc_threshold (float): cross trigger tolerance criterion threshold
+        alpha_ct (float): parameter for penalizing cross triggers.
+            More specifically, it is the weight of the cross trigger rate
+            (averaged over all other classes) that is added to the False
+            Positive Rate (FPR) yielding the effective FPR (eFPR). Default is 0.
+        alpha_st (float): parameter for penalizing instability across classes.
+            More specifically, it is the weight of the standard deviation of
+            the per-class ROCs, that is subtracted from the mean of the
+            per-class ROCs. Default is 0.
+        unit_of_time (str): the unit of time \in {second, minute, hour} to be
+            used for computation of the eFPR (which is defined as rate per unit
+            of time). Default is hour.
+        max_efpr (float): the maximum eFPR for which the system is evaluated,
+            i.e., until where the Area under PSD ROC Curve is computed.
+            Default is 100.
+        score_transform: a (non-linear) score transformation may be used before
+            thresholding to obtain a better PSD-ROC approximation [1].
+            [1] J.Ebbers, R.Serizel, and R.Haeb-Umbach
+            "Threshold-Independent Evaluation of Sound Event Detection Scores",
+            accepted for IEEE International Conference on Acoustics, Speech, and Signal Processing (ICASSP),
+            2022
+
+    Returns:
+        psds (float): Polyphonic Sound Detection Score (PSDS), i.e., the area
+            under the approximated PSD ROC Curve up to max_efpr normalized
+            by max_efpr.
+        psd_roc (tuple of 1d np.ndarrays): tuple of effective True Positive
+            Rates and effective False Positive Rates.
+        single_class_psd_rocs (dict of tuples of 1d np.ndarrays):
+            tuple of True Positive Rates and effective False Positive Rates
+            for each event class.
+
+    """
+    from psds_eval import PSDSEval
+    ground_truth = _parse_ground_truth(ground_truth)
+    audio_durations = _parse_audio_durations(audio_durations)
+    dir_path = Path(dir_path)
     psds_eval = PSDSEval(
         dtc_threshold=dtc_threshold,
         gtc_threshold=gtc_threshold,
@@ -96,36 +151,29 @@ def approximate_psds(
         metadata=audio_durations,
         duration_unit=unit_of_time,
     )
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        dir_path = Path(tmp_dir)
-        write_detections_for_multiple_thresholds(
-            scores, thresholds, dir_path, audio_format=audio_format,
-            score_transform=score_transform,
-        )
+    psds_eval.clear_all_operating_points()
 
-        psds_eval.clear_all_operating_points()
+    for i, threshold in enumerate(thresholds):
+        tsv = dir_path / '{:.3f}.tsv'.format(threshold)
+        print(f"Adding operating point {i+1}/{len(thresholds)}", end="\r")
+        det = pd.read_csv(tsv, sep="\t")
+        info = {"name": f"Op {i+1:02d}", "threshold": threshold}
+        psds_eval.add_operating_point(det, info=info)
 
-        for i, tsv in enumerate(dir_path.glob('*.tsv')):
-            print(f"Adding operating point {i+1}/{len(thresholds)}", end="\r")
-            threshold = float(tsv.name[:-len('.tsv')])
-            det = pd.read_csv(tsv, sep="\t")
-            info = {"name": f"Op {i+1:02d}", "threshold": threshold}
-            psds_eval.add_operating_point(det, info=info)
+    # compute the PSDS of the system represented by its operating points
+    psds_ = psds_eval.psds(
+        max_efpr=max_efpr,
+        alpha_st=alpha_st,
+        alpha_ct=alpha_ct,
+    )
+    efpr = psds_.plt.xp
+    etpr = psds_.plt.yp
+    _, _, tpr_vs_efpr = psds_eval.psd_roc_curves(alpha_ct=alpha_ct)
 
-        # compute the PSDS of the system represented by its operating points
-        psds_ = psds_eval.psds(
-            max_efpr=max_efpr,
-            alpha_st=alpha_st,
-            alpha_ct=alpha_ct,
-        )
-        efpr = psds_.plt.xp
-        etpr = psds_.plt.yp
-        _, _, tpr_vs_efpr = psds_eval.psd_roc_curves(alpha_ct=alpha_ct)
-
-        single_class_psd_rocs = {
-            event_classes[i]: (tpr_vs_efpr.yp[i], tpr_vs_efpr.xp)
-            for i in range(len(tpr_vs_efpr.yp))
-        }
+    single_class_psd_rocs = {
+        psds_eval.class_names[i]: (tpr_vs_efpr.yp[i], tpr_vs_efpr.xp)
+        for i in range(len(tpr_vs_efpr.yp))
+    }
 
     if max_efpr is not None:
         cutoff_idx = get_first_index_where(efpr, "gt", max_efpr)
@@ -137,3 +185,32 @@ def approximate_psds(
                 roc[0][:cutoff_idx], roc[1][:cutoff_idx]
             )
     return psds_.value, (etpr, efpr), single_class_psd_rocs
+
+
+def _parse_ground_truth(ground_truth):
+    if isinstance(ground_truth, pd.DataFrame):
+        return ground_truth
+    elif isinstance(ground_truth, dict):
+        ground_truth = [(f"{key}.wav", *gt) for key in sorted(ground_truth.keys()) for gt in ground_truth[key]]
+        ground_truth = pd.DataFrame(
+            ground_truth,
+            columns=['filename', 'onset', 'offset', 'event_label']
+        )
+    else:
+        assert isinstance(ground_truth, (str, Path)), type(ground_truth)
+        ground_truth = pd.read_csv(ground_truth, sep="\t")
+    return ground_truth
+
+
+def _parse_audio_durations(audio_durations):
+    if isinstance(audio_durations, pd.DataFrame):
+        return audio_durations
+    elif isinstance(audio_durations, dict):
+        audio_durations = [(f"{key}.wav", audio_durations[key]) for key in audio_durations.keys()]
+        audio_durations = pd.DataFrame(
+            audio_durations, columns=['filename', 'duration']
+        )
+    else:
+        assert isinstance(audio_durations, (str, Path)), type(audio_durations)
+        audio_durations = pd.read_csv(audio_durations, sep="\t")
+    return audio_durations
