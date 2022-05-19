fix true positives classified as false positives due to limited floating precision in division

sed_scores_eval/intersection_based/intermediate_statistics.py
@@ -141,21 +141,22 @@ def statistics_fn(
     )
     total_intersection_with_gt_events = np.round(
         np.sum(ground_truth_intersections, axis=-1), decimals=time_decimals)
-    detection_lengths = np.round(
-        detection_offset_times-detection_onset_times, decimals=time_decimals)
-    detection_lengths[detection_lengths == 0.] = 1e-12
-    dtc_scores = total_intersection_with_gt_events / detection_lengths
-    dtc = dtc_scores >= dtc_threshold
+    detection_lengths = detection_offset_times - detection_onset_times
+    dtc = (
+        total_intersection_with_gt_events
+        >= np.round(dtc_threshold * detection_lengths, decimals=time_decimals)
+    ) * det_crit
     num_relevant_detections = dtc.sum(-1)
     fps = num_detections - num_relevant_detections
     total_intersection_with_relevant_detections = np.round(
         np.sum(dtc[..., None] * ground_truth_intersections, axis=-2),
         decimals=time_decimals
     )
-    gt_lengths = np.round(
-        target_offset_times-target_onset_times, decimals=time_decimals)
-    gtc_scores = total_intersection_with_relevant_detections / gt_lengths
-    gtc = gtc_scores >= gtc_threshold
+    gt_lengths = target_offset_times - target_onset_times
+    gtc = (
+        total_intersection_with_relevant_detections
+        >= np.round(gtc_threshold * gt_lengths, decimals=time_decimals)
+    )
     tps = gtc.sum(-1)
     if cttc_threshold is None:
         cts = np.zeros_like(tps)
@@ -168,13 +169,13 @@ def statistics_fn(
                 0.,
             )
             total_intersection_with_other_gt_events = np.round(
-                np.sum((1-dtc[..., None])*other_class_intersections, axis=-1),
+                np.sum((1-dtc[..., None]) * other_class_intersections, axis=-1),
                 decimals=time_decimals
             )
-            cttc_scores = (
-                total_intersection_with_other_gt_events / detection_lengths
-            )
-            cttc = cttc_scores >= cttc_threshold
+            cttc = (
+                total_intersection_with_other_gt_events
+                >= np.round(cttc_threshold * detection_lengths, decimals=time_decimals)
+            ) * det_crit
             cts.append(cttc.sum(-1))
         cts = np.array(cts).T
     return {'tps': tps, 'fps': fps, 'cts': cts}

tests/test_intersection_based/test_psds.py
@@ -13,14 +13,19 @@ from sed_scores_eval import intersection_based, io, package_dir
             'gtc_threshold': .7,
             'cttc_threshold': None,
             'alpha_ct': .0,
-            'alpha_st': 1 / np.sqrt(10 - 1) - 1e-9,
+            'alpha_st': 1 / np.sqrt(10 - 1),
+            # choose alpha_st <= 1/sqrt(K-1), where K is the number of classes,
+            # to ensure that the true PSD-ROC is always greater than an
+            # approximation. With alpha_st > 1/sqrt(K-1) an increased TPR for
+            # one of the classes may cause the effective TPR over all classes
+            # to decrease.
         },
         {
             'dtc_threshold': .1,
             'gtc_threshold': .1,
             'cttc_threshold': .3,
             'alpha_ct': .5,
-            'alpha_st': 1 / np.sqrt(10 - 1) - 1e-9,
+            'alpha_st': 1 / np.sqrt(10 - 1),
         },
     ]
 )
@@ -38,8 +43,7 @@ def test_psds_vs_psds_eval(dataset, params):
         gtc_threshold=params['gtc_threshold'],
         cttc_threshold=params['cttc_threshold'],
         alpha_ct=params['alpha_ct'], alpha_st=params['alpha_st'],
-        unit_of_time='hour', max_efpr=100.,
-        time_decimals=30, num_jobs=8,
+        unit_of_time='hour', max_efpr=100., num_jobs=8, time_decimals=6,
     )
 
     score_transform = test_data_dir / 'validation' / 'score_transform.tsv'
@@ -78,6 +82,7 @@ def test_psds_vs_psds_eval(dataset, params):
         assert (tpr_true >= tpr_approx).all(), (
             class_name,
             np.sum(tpr_true < tpr_approx),
+            len(tpr_true),
             (tpr_approx - tpr_true).max(),
         )
 