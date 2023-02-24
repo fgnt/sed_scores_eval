a2ed3862b270e504a1025bdc76625920040513ab
Janek Ebbers <ebbers@nt.upb.de>
2023-02-24 09:48:38 +0100

    imports to upper namespace

--- sed_scores_eval/__init__.py
+++ sed_scores_eval/__init__.py
@@ -1,4 +1,8 @@
 from pathlib import Path
 from .base_modules import io
+from . import intersection_based
+from . import collar_based
+from . import segment_based
+from . import utils
 
 package_dir = Path(__file__).parent.parent.absolute()
