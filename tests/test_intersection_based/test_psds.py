b147ec5fd43ef21ff771fbb03af8874bdfbe7974
Janek Ebbers <ebbers@nt.upb.de>
2024-03-29 09:33:41 -0400

    add tests

--- tests/test_intersection_based/test_psds.py
+++ tests/test_intersection_based/test_psds.py
@@ -4,6 +4,7 @@ from sed_scores_eval.base_modules.bootstrap import confidence_interval
 from sed_scores_eval.utils.test import assert_roc_geq_roc_ref
 from sed_scores_eval import io, package_dir
 from sed_scores_eval import intersection_based
+from sed_scores_eval.base_modules.scores import validate_score_dataframe, create_score_dataframe
 
 
 @pytest.mark.parametrize("dataset", ["eval"])
@@ -75,6 +76,76 @@ def test_psds_vs_psds_eval(dataset, params, num_jobs):
             event_class,
             upper_bound=2e-2,
         )
+
+
+@pytest.mark.parametrize("dataset", ["eval"])
+@pytest.mark.parametrize(
+    "params",
+    [
+        {
+            'dtc_threshold': .7,
+            'gtc_threshold': .7,
+            'cttc_threshold': None,
+            'alpha_ct': .0,
+            'alpha_st': 1 / np.sqrt(10 - 1),
+            # choose alpha_st <= 1/sqrt(K-1), where K is the number of classes,
+            # to ensure that the true PSD-ROC is always greater than an
+            # approximation. With alpha_st > 1/sqrt(K-1) an increased TPR for
+            # one of the classes may cause the effective TPR over all classes
+            # to decrease.
+        },
+        {
+            'dtc_threshold': .1,
+            'gtc_threshold': .1,
+            'cttc_threshold': .3,
+            'alpha_ct': .5,
+            'alpha_st': 1 / np.sqrt(10 - 1),
+        },
+    ]
+)
+@pytest.mark.parametrize("num_jobs", [1, 2])
+def test_psds_repeated_scores(dataset, params, num_jobs):
+
+    test_data_dir = package_dir / 'tests' / 'data'
+    if not test_data_dir.exists():
+        io.download_test_data()
+
+    scores = io.read_sed_scores(test_data_dir / dataset / "scores")
+    scores, ground_truth, audio_ids = io.parse_inputs(scores, test_data_dir / dataset / "ground_truth.tsv")
+    audio_durations = io.parse_audio_durations(test_data_dir / dataset / "audio_durations.tsv", audio_ids=audio_ids)
+    psds_ref, single_class_psds_ref, psd_roc_ref, single_class_psd_rocs_ref = intersection_based.psds(
+        scores=scores,
+        ground_truth=ground_truth,
+        audio_durations=audio_durations,
+        dtc_threshold=params['dtc_threshold'],
+        gtc_threshold=params['gtc_threshold'],
+        cttc_threshold=params['cttc_threshold'],
+        alpha_ct=params['alpha_ct'], alpha_st=params['alpha_st'],
+        unit_of_time='hour', max_efpr=100., time_decimals=6, num_jobs=num_jobs,
+    )
+    def _repeat_scores(scores_df):
+        timestamps, event_classes = validate_score_dataframe(scores_df)
+        scores_arr = scores_df[event_classes].to_numpy()
+        scores_arr = np.repeat(scores_arr, 2, axis=0)
+        timestamps = np.concatenate((
+            np.stack((timestamps[:-1], (timestamps[1:]+timestamps[:-1])/2), axis=-1).flatten(),
+            timestamps[-1:]
+        ))
+        idx = (np.arange(1, len(timestamps)+1) % 4) > 0
+        return create_score_dataframe(scores_arr[idx[:-1]], timestamps[idx], event_classes)
+    scores_repeated = {key: _repeat_scores(scores_df) for key, scores_df in scores.items()}
+    psds, single_class_psds, psd_roc, single_class_psd_rocs = intersection_based.psds(
+        scores=scores_repeated,
+        ground_truth=ground_truth,
+        audio_durations=audio_durations,
+        dtc_threshold=params['dtc_threshold'],
+        gtc_threshold=params['gtc_threshold'],
+        cttc_threshold=params['cttc_threshold'],
+        alpha_ct=params['alpha_ct'], alpha_st=params['alpha_st'],
+        unit_of_time='hour', max_efpr=100., time_decimals=6, num_jobs=num_jobs,
+    )
+    assert abs(psds - psds_ref) < 1e-6, (psds, psds_ref)
+
 
 
 @pytest.mark.parametrize("dataset", ["eval"])
--- tests/test_utils/test_median_filter.py
+++ tests/test_utils/test_median_filter.py
@@ -4,7 +4,8 @@ import pytest
 import time
 from sed_scores_eval.base_modules.cy_medfilt import cy_medfilt
 from sed_scores_eval.base_modules.postprocessing import medfilt
-from sed_scores_eval.base_modules.scores import create_score_dataframe
+from sed_scores_eval.base_modules.scores import create_score_dataframe, validate_score_dataframe
+from sed_scores_eval.base_modules.detection import scores_to_event_list
 from sed_scores_eval import io, package_dir
 
 
@@ -96,3 +97,51 @@ def test_real_data(dataset):
         change_points = np.argwhere((scores_ref != prev_scores).any(-1)).flatten()
         scores_ref = scores_ref[change_points]
         assert (scores_i.to_numpy()[:,2:] == scores_ref).all()
+
+
+@pytest.mark.parametrize("dataset", ["validation", "eval"])
+def test_medfilt_lengths_dict(dataset):
+    test_data_dir = package_dir / 'tests' / 'data'
+    if not test_data_dir.exists():
+        io.download_test_data()
+
+    median_filter_length_in_sec = {
+        'Alarm_bell_ringing': 0.02,
+        'Blender': 0.02,
+        'Cat': 0.2,
+        'Dishes': 0.02,
+        'Dog': 0.2,
+        'Electric_shaver_toothbrush': 0.4,
+        'Frying': 0.2,
+        'Running_water': 0.6,
+        'Speech': 0.2,
+        'Vacuum_cleaner': 1.6,
+    }
+    event_classes = sorted(median_filter_length_in_sec.keys())
+    scores = io.read_sed_scores(test_data_dir / dataset / "scores")
+    median_filter_length_in_sec_arr = np.array([median_filter_length_in_sec[event_class] for event_class in event_classes])
+    scores_medfiltered = medfilt(scores, median_filter_length_in_sec_arr)
+    for audio_id, scores_i in scores_medfiltered.items():
+        timestamps_orig, _ = validate_score_dataframe(scores[audio_id], event_classes=event_classes)
+        timestamps, _ = validate_score_dataframe(scores_i, event_classes=event_classes)
+        for event_class in event_classes:
+            scores_ref, timestamps_ref = cy_medfilt(scores[audio_id][[event_class]].to_numpy(), timestamps_orig, median_filter_length_in_sec[event_class])
+            scores_ref = scores_ref[:,0]
+            scores_ic = scores_i[event_class].to_numpy()
+            indices = [0] + (np.argwhere(scores_ic[1:] != scores_ic[:-1]).flatten()+1).tolist()
+            timestamps_ic = timestamps[indices + [-1]]
+            scores_ic = scores_ic[indices]
+            assert len(timestamps_ic) == len(timestamps_ref), (len(timestamps_ic), len(timestamps_ref))
+            assert (timestamps_ic == timestamps_ref).all()
+            assert len(scores_ic) == len(scores_ref), (len(scores_ic), len(scores_ref))
+            assert (scores_ic == scores_ref).all()
+
+    detections_ref = {}
+    for fillen in sorted(set(median_filter_length_in_sec.values())):
+        detections_ref[fillen] = scores_to_event_list(medfilt(scores, fillen), .5)
+    detections = scores_to_event_list(scores_medfiltered, .5)
+    for clip_id in detections:
+        events_ref = sorted([event for fillen in detections_ref for event in detections_ref[fillen][clip_id] if median_filter_length_in_sec[event[2]] == fillen])
+        events = sorted(detections[clip_id])
+        assert len(events) == len(events_ref), (len(events), len(events_ref))
+        assert events == events_ref, (events, events_ref)
